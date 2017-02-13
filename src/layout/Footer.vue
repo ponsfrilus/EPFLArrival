c5c86af388e6b73a32fe139e5ffb8c91a44cd07f
ponsfrilus <ponsfrilus@gmail.com>
2017-02-13 16:17:31 +0100

    Fix link + updated info

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <footer>
-    &mdash; <small>Proudly using <a href="https://vuejs.org/v2/">vue.js</a> and made with <a href="https://en.wikipedia.org/wiki/Open-source_software">oss</a> by <a href="https://go.epfl.ch/nbo">nbo</a> | Submit new tiles with <acronym title="Pull Request" lang="en">PR</acronym> on <a>href="https://gitlab.epfl.ch/###">gitlab</a></small> &mdash;
+    &mdash; <small>Proudly using <a href="https://vuejs.org/v2/">vue.js</a> based on an idea of <a href="https://go.epfl.ch/nbo">nbo</a> with help of <a href="https://github.com/epfl-dojo">EPFLDojo</a></small> &mdash;<br />
+    <small>Submit new tiles with <acronym title="Pull Request" lang="en">PR</acronym> on <a href="https://github.com/ponsfrilus/EPFLArrival">GitHub</a></small>
   </footer>
 </template>

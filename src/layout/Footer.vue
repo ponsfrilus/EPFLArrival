d2da982f6b25fc6f177b7a39435cc5a1f5cf96d5
ponsfrilus <ponsfrilus@gmail.com>
2017-02-16 02:10:32 +0100

    Inspiring stargazers with https://buttons.github.io

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,6 +1,10 @@
 <template>
   <footer>
     &mdash; <small>Proudly using <a href="https://vuejs.org/v2/">vue.js</a> based on an idea of <a href="https://go.epfl.ch/nbo">nbo</a> with help of <a href="https://github.com/epfl-dojo">EPFLDojo</a></small> &mdash;<br />
-    <small>Submit new tiles with <acronym title="Pull Request" lang="en">PR</acronym> on <a href="https://github.com/ponsfrilus/EPFLArrival">GitHub</a></small>
+    <small>Submit new tiles with <acronym title="Pull Request" lang="en">PR</acronym> on <a href="https://github.com/ponsfrilus/EPFLArrival/blob/master/CONTRIBUTING.md">GitHub</a></small><br />
+    <!-- https://buttons.github.io/ -->
+    <a class="github-button" href="https://github.com/ponsfrilus/EPFLArrival/fork" data-icon="octicon-repo-forked" aria-label="Fork ponsfrilus/EPFLArrival on GitHub">Fork</a>
+    <a class="github-button" href="https://github.com/ponsfrilus/EPFLArrival" data-icon="octicon-star" data-count-href="/ponsfrilus/EPFLArrival/stargazers" data-count-api="/repos/ponsfrilus/EPFLArrival#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star ponsfrilus/EPFLArrival on GitHub">Star</a>
+    <a class="github-button" href="https://github.com/ponsfrilus" aria-label="Follow @ponsfrilus on GitHub">Follow</a>
   </footer>
 </template>

90125c48c4a045864ed563848a25ba25d410861e
ponsfrilus <ponsfrilus@gmail.com>
2017-02-16 11:37:12 +0100

    Import the infowww tile

--- build/dev-server.js
+++ build/dev-server.js
@@ -63,8 +63,6 @@ app.use(hotMiddleware)
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
-app.get('/tiles.json', (req, res) => { res.sendFile(path.join(__dirname, '../data/tiles.json')); });
-
 
 var uri = 'http://localhost:' + port
 

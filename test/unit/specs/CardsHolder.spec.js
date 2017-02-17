a6b686c7c83320862b888453f9f94c4ad3fecfb1
ponsfrilus <ponsfrilus@gmail.com>
2017-02-17 16:30:25 +0100

    Linter problem fixed

--- test/unit/specs/CardsHolder.spec.js
+++ test/unit/specs/CardsHolder.spec.js
@@ -7,22 +7,24 @@ describe('CardsHolder.vue', () => {
       el: document.createElement('div'),
       render: (createElement) => createElement(CardsHolder, {
         props:
-          {infos: [
+        {
+          'infos': [
             {
-      "title":"SDF",
-      "desc_fr":"Site officiel de la SDF",
-      "desc_en":"Official SDF website",
-      "link":"http://studdevf.epfl.ch",
-      "tags":[
-        "sdf",
-        "students",
-        "website"
-      ],
-      "maincat":[
-        "communication"
-      ]
-    },
-          ]}
+              'title': 'SDF',
+              'desc_fr': 'Site officiel de la SDF',
+              'desc_en': 'Official SDF website',
+              'link': 'http://studevf.epfl.ch',
+              'tags': [
+                'sdf',
+                'students',
+                'website'
+              ],
+              'maincat': [
+                'communication'
+              ]
+            }
+          ]
+        }
       })
     })
     expect(vm.$el.querySelector('div.searchCategories div.check-communication').textContent)
